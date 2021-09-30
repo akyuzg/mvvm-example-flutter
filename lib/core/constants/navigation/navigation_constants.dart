285204f19beae6f3aa7716c4523a2936573d6046
gurayakyuz <gur.akyuz@gmail.com>
2021-09-30 16:44:03 +0300

    add some route const

--- lib/core/constants/navigation/navigation_constants.dart
+++ lib/core/constants/navigation/navigation_constants.dart
@@ -1,6 +1,7 @@
-class NavigationConstants {
-  // ignore: constant_identifier_names
-  static const TEST_VIEW = '/test';
-  // ignore: constant_identifier_names
+class NavigationContants {
   static const DEFAULT = '/';
+  static const TEST_VIEW = '/test';
+  static const TEST_WITH_BUTTON_VIEW = '/test-button';
+  static const HOME_VIEW = '/home';
+  static const NOT_FOUND = '/notfound';
 }
