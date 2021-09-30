626c3815acc8021c208ad5129b181fede1323f97
gurayakyuz <gur.akyuz@gmail.com>
2021-09-30 17:15:05 +0300

    teller model updated

--- lib/features/home/advisor/model/teller_model.dart
+++ lib/features/home/advisor/model/teller_model.dart
@@ -4,20 +4,16 @@ import 'package:mvvm_example_flutter/core/base/base_model.dart';
 part 'teller_model.g.dart';
 
 @JsonSerializable()
-class TellerModel extends BaseModel<TellerModel>{
+class TellerModel extends BaseModel<TellerModel> {
   String? name;
   TellerModel({this.name});
 
-  TellerModel.fromJson(Map<String, dynamic> json) {
-    name = json['name'] as String?;
-  }
-
-@override
+  @override
   Map<String, dynamic> toJson() {
     return _$TellerModelToJson(this);
   }
 
-@override
+  @override
   TellerModel fromJson(Map<String, Object?> json) {
     return _$TellerModelFromJson(json);
   }
