2897b76c62f45448251427fb18c2599b6a796c75
gurayakyuz <gur.akyuz@gmail.com>
2021-09-30 16:48:43 +0300

    simple theme data for global theme parameters

--- lib/core/init/theme/light/theme_data.dart
+++ lib/core/init/theme/light/theme_data.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 
 const String fontFamily = "Poppins";
 
-final ThemeData themeData = ThemeData(
-    // Define the default brightness and colors.
-    brightness: Brightness.dark,
-    primaryColor: Colors.lightBlue[800],
-
-    // Define the default font family.
-    fontFamily: fontFamily,
-
-    // Define the default `TextTheme`. Use this to specify the default
-    // text styling for headlines, titles, bodies of text, and more.
-    textTheme: const TextTheme(
-      headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-      headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-      bodyText2: TextStyle(fontSize: 14.0),
-      bodyText1: TextStyle(fontSize: 12.0),
-    ),
-
-   
-  );
+final ThemeData themeData  = ThemeData(
+      //splashColor: const Color(0xFF804FCC),
+      brightness: Brightness.light,
+      primaryColor: const Color(0xFF804FCC),
+      textTheme: const TextTheme(
+        headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+        bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+      ),
+      colorScheme: const ColorScheme(
+          primary: Color(0xFF804FCC),
+          primaryVariant: Color(0xFFC0C0C0),
+          secondary: Color(0xFFFFFFFF),
+          secondaryVariant: Color(0xFFC0C0C0),
+          surface: Color(0xFFC0C0C0),
+          background: Color(0xFFC0C0C0),
+          error: Color(0xFFC0C0C0),
+          onPrimary: Color(0xFFC0C0C0),
+          onSecondary: Color(0xFFC0C0C0),
+          onSurface: Color(0xFFC0C0C0),
+          onBackground: Color(0xFFC0C0C0),
+          onError: Color(0xFFC0C0C0),
+          brightness: Brightness.light));
