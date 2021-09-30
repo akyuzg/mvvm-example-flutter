f6a80153e4204b31f9d22e3f4741405dd0d90632
gurayakyuz <gur.akyuz@gmail.com>
2021-09-30 16:37:39 +0300

    tab enum and tabNames constant

---
+++ lib/core/constants/enums/tab_enum.dart
@@ -0,0 +1,1 @@
+enum TabItem { home, store, campaign, profile }
---
+++ lib/core/constants/navigation/bottom_navigation_constants.dart
@@ -0,0 +1,8 @@
+import 'package:mvvm_example_flutter/core/constants/enums/tab_enum.dart';
+
+const Map<TabItem, String> tabName = {
+  TabItem.home: 'home',
+  TabItem.store: 'store',
+  TabItem.campaign: 'campaign',
+  TabItem.profile: 'profile',
+};
