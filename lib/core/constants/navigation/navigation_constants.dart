4186291de9dfbc504736fc2f729918c7889788b8
gurayakyuz <gur.akyuz@gmail.com>
2021-10-03 20:32:50 +0300

    nav constants variable name changed camel case notation

--- lib/core/constants/navigation/navigation_constants.dart
+++ lib/core/constants/navigation/navigation_constants.dart
@@ -1,7 +1,7 @@
 class NavigationContants {
-  static const DEFAULT = '/';
-  static const TEST_VIEW = '/test';
-  static const TEST_WITH_BUTTON_VIEW = '/test-button';
-  static const HOME_VIEW = '/home';
-  static const NOT_FOUND = '/notfound';
+  static const navDefault = '/';
+  static const navTest = '/test';
+  static const navTestWithButton = '/test-button';
+  static const navMain = '/main';
+  static const navNotFound = '/notfound';
 }
