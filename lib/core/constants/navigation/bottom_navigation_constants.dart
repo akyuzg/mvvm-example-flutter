107b3509f51d5364731889f617ae2ae14c802f38
gurayakyuz <gur.akyuz@gmail.com>
2021-10-03 20:32:18 +0300

    tabIcons map added

--- lib/core/constants/navigation/bottom_navigation_constants.dart
+++ lib/core/constants/navigation/bottom_navigation_constants.dart
@@ -1,6 +1,13 @@
 import 'package:mvvm_example_flutter/core/constants/enums/tab_enum.dart';
 
 const Map<TabItem, String> tabName = {
+  TabItem.home: 'Home',
+  TabItem.store: 'Store',
+  TabItem.campaign: 'Campaign',
+  TabItem.profile: 'Profile',
+};
+
+const Map<TabItem, String> tabIcons = {
   TabItem.home: 'home',
   TabItem.store: 'store',
   TabItem.campaign: 'campaign',
