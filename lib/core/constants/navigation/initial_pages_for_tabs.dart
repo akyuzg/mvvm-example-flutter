e57b899b7b1b6ed8466a840fd566c21a0d658a97
gurayakyuz <gur.akyuz@gmail.com>
2021-10-03 20:35:43 +0300

    inital page defined for tabs

---
+++ lib/core/constants/navigation/initial_pages_for_tabs.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_example_flutter/core/constants/enums/tab_enum.dart';
+import 'package:mvvm_example_flutter/features/_product/common/test_page_widget.dart';
+import 'package:mvvm_example_flutter/features/_product/common/test_page_with_button.dart';
+
+class InitialPages {
+  static const Map<TabItem, Widget> _pages = {
+    TabItem.home: TestPageWidget(),
+    TabItem.store: TestPageWidget(),
+    TabItem.campaign: TestPageWithButtonWidget(),
+    TabItem.profile: TestPageWithButtonWidget()
+  };
+
+  static getPage(TabItem item) => _pages[item];
+}
