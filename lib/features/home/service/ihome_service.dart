366da52f57681f000eebddddd2011d0c37895bc1
gurayakyuz <gur.akyuz@gmail.com>
2021-10-09 00:26:33 +0300

    home service abstract class for network injection

--- lib/features/home/service/ihome_service.dart
+++ lib/features/home/service/ihome_service.dart
@@ -1,5 +1,4 @@
 import 'package:mvvm_example_flutter/core/init/network/network_manager.dart';
-import 'package:mvvm_example_flutter/features/home/service/home_service.dart';
 
 abstract class IHomeService {
   NetworkManager networkManager = NetworkManager.getInstance();
