5e6d040d89f288ab2957ce4cce0727cd8630cec9
gurayakyuz <gur.akyuz@gmail.com>
2021-10-08 22:57:05 +0300

    networkManager added to baseviewmodel

--- lib/core/base/base_view_model.dart
+++ lib/core/base/base_view_model.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_example_flutter/core/init/navigation/navigation_service.dart';
+import 'package:mvvm_example_flutter/core/init/network/network_manager.dart';
 
-abstract class BaseViewModel{
-
+abstract class BaseViewModel {
   NavigationService navigation = NavigationService.instance;
+  NetworkManager networkManager = NetworkManager.getInstance();
 
   late BuildContext context;
   void setContext(BuildContext context);
