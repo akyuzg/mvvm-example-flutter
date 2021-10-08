50edd0910fd3e218e18fa818e7ff6038d121e918
gurayakyuz <gur.akyuz@gmail.com>
2021-10-09 00:25:44 +0300

    splash test

--- test/service/splash_service_test.dart
+++ test/service/splash_service_test.dart
@@ -1,6 +1,7 @@
-import 'package:mvvm_example_flutter/core/init/network/network_manager.dart';
-import 'package:mvvm_example_flutter/features/authenticate/splash/service/splash_service.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mvvm_example_flutter/core/init/network/network_manager.dart';
+import 'package:mvvm_example_flutter/features/authenticate/splash/model/splash_response.dart';
+import 'package:mvvm_example_flutter/features/authenticate/splash/service/splash_service.dart';
 
 void main() {
   SplashService? splashService;
@@ -8,9 +9,8 @@ void main() {
     splashService = SplashService(NetworkManager.getInstance());
   });
   test('Fetch splash', () async {
-    final response = await splashService?.getSplash();
-
+    SplashResponse? response = await splashService?.getSplash();
     expect(response, isNotNull);
-    expect(response?.data, isNotNull);
+    expect(response?.status, 200);
   });
 }
