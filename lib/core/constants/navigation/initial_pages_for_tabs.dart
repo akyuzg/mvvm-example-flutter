1d120427dbd8f0efaa0d888d57cffab0de8b560e
gurayakyuz <gur.akyuz@gmail.com>
2021-10-08 20:57:58 +0300

    first tab changed with homeview

--- lib/core/constants/navigation/initial_pages_for_tabs.dart
+++ lib/core/constants/navigation/initial_pages_for_tabs.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:mvvm_example_flutter/core/constants/enums/tab_enum.dart';
 import 'package:mvvm_example_flutter/features/_product/common/test_page_widget.dart';
 import 'package:mvvm_example_flutter/features/_product/common/test_page_with_button.dart';
+import 'package:mvvm_example_flutter/features/home/view/home_view.dart';
 
 class InitialPages {
   static const Map<TabItem, Widget> _pages = {
-    TabItem.home: TestPageWidget(),
+    TabItem.home: HomeView(),
     TabItem.store: TestPageWidget(),
     TabItem.campaign: TestPageWithButtonWidget(),
     TabItem.profile: TestPageWithButtonWidget()
