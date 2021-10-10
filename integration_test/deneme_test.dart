e6090bede457b3a82352942eb49c1ace03261630
gurayakyuz <gur.akyuz@gmail.com>
2021-10-10 21:06:53 +0300

    Update deneme_test

--- integration_test/deneme_test.dart
+++ integration_test/deneme_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:mvvm_example_flutter/core/constants/enums/tab_enum.dart';
+import 'package:mvvm_example_flutter/core/constants/navigation/bottom_navigation_constants.dart';
 import 'package:mvvm_example_flutter/main.dart';
 
 void main() {
@@ -9,7 +11,9 @@ void main() {
   testWidgets("A integration test example", (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
     await tester.pumpAndSettle(const Duration(seconds: 5));
-    await tester.tap(find.byKey(const Key('store')));
+
+    //Open Store Tab
+    await tester.tap(find.byKey(Key(tabName[TabItem.store]!)));
     await tester.pumpAndSettle();
     expect(find.text("test page"), findsOneWidget);
   });
