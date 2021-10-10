129ef95db9605d60d00f0dca96e429209c6ec943
gurayakyuz <gur.akyuz@gmail.com>
2021-10-10 21:12:59 +0300

    create decodeErrorResponse for all api calls

--- lib/core/init/network/network_manager.dart
+++ lib/core/init/network/network_manager.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:mvvm_example_flutter/core/constants/network/network_contants.dart';
 
@@ -7,6 +10,15 @@ class NetworkManager {
   static NetworkManager? get instance {
     _instance ?? NetworkManager._init();
     return _instance;
+  }
+
+  dynamic get(String path) async {
+    try {
+      Response raw = (await dio.get(path));
+      return raw.data;
+    } on DioError catch (e) {
+      return decodeErrorResponse(e);
+    }
   }
 
   factory NetworkManager.getInstance() {
@@ -21,5 +33,32 @@ class NetworkManager {
     final baseOptions =
         BaseOptions(baseUrl: NetworkConstants.baseUrl, headers: {});
     dio = Dio(baseOptions);
+  }
+
+  dynamic decodeErrorResponse(dynamic e) {
+    var data = {"status": -1, "message": "Unknown Error"};
+    if (e is DioError) {
+      if (e.type == DioErrorType.response) {
+        final response = e.response;
+        try {
+          if (response != null && response.data != null) {
+            final Map responseData =
+                json.decode(response.data as String) as Map;
+            data["message"] = responseData['message'] as String;
+            data["status"] = response.statusCode!;
+          }
+        } catch (e) {
+          data["message"] = "Internal Error Catch";
+        }
+      } else if (e.type == DioErrorType.connectTimeout ||
+          e.type == DioErrorType.receiveTimeout ||
+          e.type == DioErrorType.sendTimeout) {
+        data["message"] = "Request timeout";
+        data["status"] = 408;
+      } else if (e.error is SocketException) {
+        data["message"] = "No Internet Connection!";
+      }
+    }
+    return data;
   }
 }
--- lib/features/home/service/home_service.dart
+++ lib/features/home/service/home_service.dart
@@ -1,16 +1,13 @@
-import 'package:dio/dio.dart';
 import 'package:mvvm_example_flutter/core/constants/network/network_contants.dart';
 import 'package:mvvm_example_flutter/core/init/network/network_manager.dart';
-import 'package:mvvm_example_flutter/features/home/advisor/model/advisor_model.dart';
 import 'package:mvvm_example_flutter/features/home/model/response/advisors_response.dart';
 import 'package:mvvm_example_flutter/features/home/service/ihome_service.dart';
 
 class HomeService extends IHomeService {
   HomeService(NetworkManager manager) : super(manager);
 
-  Future<List<AdvisorModel>?> fetchAdvisors() async {
-    Response raw = (await networkManager.dio.get(NetworkConstants.getAdvisors));
-    AdvisorsResponse response = AdvisorsResponse.fromJson(raw.data);
-    return response.advisors;
+  Future<dynamic> fetchAdvisors() async {
+    final data = await networkManager.get(NetworkConstants.getAdvisors);
+    return AdvisorsResponse.fromJson(data);
   }
 }
